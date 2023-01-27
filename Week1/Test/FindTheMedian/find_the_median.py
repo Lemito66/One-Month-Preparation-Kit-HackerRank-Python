115a77f77f6c519d30676097b5f58147f4e36d46
Emill <lemito66@gmail.com>
2023-01-27 10:19:42 -0500

    listo la mediana

--- Week1/Test/FindTheMedian/find_the_median.py
+++ Week1/Test/FindTheMedian/find_the_median.py
@@ -6,3 +6,14 @@ def find_the_median(list_of_numbers:list):
 
 
 print(find_the_median([8,6,5,7,9]))
+
+def metodo_burbuja(list_of_numbers):
+    for i in range(len(list_of_numbers)):
+        for j in range(len(list_of_numbers)-1):
+            if list_of_numbers[j] > list_of_numbers[j+1]:
+                number_to_replace = list_of_numbers[j]
+                list_of_numbers[j] = list_of_numbers[j+1]
+                list_of_numbers[j+1] = number_to_replace
+    return list_of_numbers
+
+print(metodo_burbuja([8,6,5,7,9]))
